67c954fb910a56f81f16ff8a65a385eb488e2b4f
Natnael Abrham <70650725+natnaelabrham@users.noreply.github.com>
2024-02-28 13:26:53 +0300

    added text styling
    added text styling

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: Text('Flutter Basics'),
+      body: Text(
+        'Flutter Basics',
+        style: TextStyle(fontSize: 20),
+      ),
     ),
   ));
 }
